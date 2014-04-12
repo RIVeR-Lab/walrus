c740c8220a0034b2ccb2176a07ba274583175fef
Mitchell Wills <mwills@wpi.edu>
2014-04-11 21:17:56 -0400

    Added udev rules Implemented teensy comm protocol

--- walrus_beaglebone_hw/include/pwm_effort_channel.h
+++ walrus_beaglebone_hw/include/pwm_effort_channel.h
@@ -1,16 +1,30 @@
+#ifndef PWM_EFFORT_CHANNEL_H_
+#define PWM_EFFORT_CHANNEL_H_
+
 #include <hardware_interface/robot_hw.h>
 #include <boost/shared_ptr.hpp>
 #include <transmission_interface/transmission_interface.h>
 #include <river_ros_util/ros_control_util.h>
+#include <device_driver_base/serial_port.h>
+#include "walrus_teensy_interface.h"
+#include <algorithm>
+#include <math.h>
 
 using namespace transmission_interface;
-class PWMEffortChannelBase : public river_ros_util::RobotHWActuator{
+using namespace device_driver;
+
+static const double us_per_torque = 10;
+static const double ticks_per_rotation = 10000;
+
+class PWMEffortChannelBase : public river_ros_util::RobotHWComponent{
  public:
-  PWMEffortChannelBase(std::string name,
+   PWMEffortChannelBase(std::string name, uint8_t channel, double max_effort,
 			   hardware_interface::ActuatorStateInterface& jsi,
-		       hardware_interface::EffortActuatorInterface& jei):
-  cmd(0), pos(0), vel(0), eff(0), name(name){
- 
+		       hardware_interface::EffortActuatorInterface& jei, WalrusTeensyInterfacePtr& teensy):
+  cmd(0), pos(0), vel(0), eff(0), name(name), channel(channel), teensy(teensy), max_effort(max_effort){
+    ROS_INFO_STREAM("CREATED: "<<name);
+    printf("created\n");
+
     hardware_interface::ActuatorStateHandle state_handle(name, &pos, &vel, &eff);
     jsi.registerHandle(state_handle);
 
@@ -21,17 +35,13 @@ class PWMEffortChannelBase : public river_ros_util::RobotHWActuator{
   virtual void read() = 0;
 
   void write(){
-    ROS_INFO_STREAM_THROTTLE(5, "output[" << name<<"] = "<<cmd);
-    eff = cmd;
-    //TODO actually write to device
-  }
-
-  virtual ActuatorData transmissionData(){
-    ActuatorData data;
-    data.position.push_back(&pos);
-    data.velocity.push_back(&vel);
-    data.effort.push_back(&eff);
-    return data;
+    ROS_INFO_THROTTLE(1, "HI");
+    double actual_cmd = std::min(max_effort, std::max(cmd, -max_effort));
+    uint16_t period_us = 1500 + us_per_torque*actual_cmd;
+    teensy->set_pwm(channel, period_us);
+    
+    eff = actual_cmd;
+    ROS_INFO_THROTTLE(1, "BYE");
   }
 
  protected:
@@ -40,19 +50,25 @@ class PWMEffortChannelBase : public river_ros_util::RobotHWActuator{
   double vel;
   double eff;
   std::string name;
+  uint8_t channel;
+  double max_effort;
+  WalrusTeensyInterfacePtr teensy;
 };
 typedef boost::shared_ptr<PWMEffortChannelBase> PWMEffortChannelBasePtr;
 
 
 class PWMPositionEffortChannel : public PWMEffortChannelBase{
  public:
-  PWMPositionEffortChannel(std::string name,
+  PWMPositionEffortChannel(std::string name, uint8_t channel, double max_effort,
 			   hardware_interface::ActuatorStateInterface& jsi,
-			       hardware_interface::EffortActuatorInterface& jei):
-  PWMEffortChannelBase(name, jsi, jei)
-{ }
+			   hardware_interface::EffortActuatorInterface& jei,
+                           WalrusTeensyInterfacePtr& teensy):
+  PWMEffortChannelBase(name, channel, max_effort, jsi, jei, teensy)
+{}
 
 	virtual void read(){
+          uint16_t raw_position = teensy->get_analog(channel);
+          pos =  2*M_PI * raw_position / 1023;
 	}
 };
 
@@ -61,15 +77,22 @@ typedef boost::shared_ptr<PWMPositionEffortChannel> PWMPositionEffortChannelPtr;
 
 class PWMVelocityEffortChannel : public PWMEffortChannelBase{
  public:
-  PWMVelocityEffortChannel(std::string name,
+  PWMVelocityEffortChannel(std::string name, uint8_t channel, double max_effort,
 			   hardware_interface::ActuatorStateInterface& jsi,
-			       hardware_interface::EffortActuatorInterface& jei):
-  PWMEffortChannelBase(name, jsi, jei)
-{ }
+			   hardware_interface::EffortActuatorInterface& jei,
+                           WalrusTeensyInterfacePtr& teensy):
+  PWMEffortChannelBase(name, channel, max_effort, jsi, jei, teensy)
+{}
 
 	virtual void read(){
+          uint16_t raw_position = teensy->get_position(channel);
+          uint16_t raw_velocity = teensy->get_velocity(channel);
+          pos =  2*M_PI * raw_position / ticks_per_rotation;
+          vel =  2*M_PI * raw_velocity / ticks_per_rotation;
 	}
 };
 
 
 typedef boost::shared_ptr<PWMVelocityEffortChannel> PWMVelocityEffortChannelPtr;
+
+#endif
--- walrus_beaglebone_hw/include/walrus_base_hw.h
+++ walrus_beaglebone_hw/include/walrus_base_hw.h
@@ -1,3 +1,6 @@
+#ifndef WALRUS_BASE_HW_H_
+#define WALRUS_BASE_HW_H_
+
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/actuator_command_interface.h>
@@ -9,27 +12,38 @@
 #include "pwm_effort_channel.h"
 #include <river_ros_util/ros_util.h>
 #include <river_ros_util/ros_control_util.h>
+#include <device_driver_base/serial_port.h>
+#include "walrus_teensy_interface.h"
 
 using namespace transmission_interface;
+using namespace device_driver;
 
 class WalrusBaseRobot : public river_ros_util::AbstractRobotHW
 {
 public:
-  WalrusBaseRobot(ros::NodeHandle n = ros::NodeHandle(), std::string robot_ns="walrus/"){
-   add_actuator(PWMPositionEffortChannelPtr(new PWMPositionEffortChannel(robot_ns+"front_left_pod_joint_actuator", as_interface, ae_interface)));
-   add_actuator(PWMPositionEffortChannelPtr(new PWMPositionEffortChannel(robot_ns+"back_left_pod_joint_actuator", as_interface, ae_interface)));
-   add_actuator(PWMPositionEffortChannelPtr(new PWMPositionEffortChannel(robot_ns+"front_right_pod_joint_actuator", as_interface, ae_interface)));
-   add_actuator(PWMPositionEffortChannelPtr(new PWMPositionEffortChannel(robot_ns+"back_right_pod_joint_actuator", as_interface, ae_interface)));
+ WalrusBaseRobot(ros::NodeHandle n = ros::NodeHandle(), std::string robot_ns="walrus/"):
+    teensy(new WalrusTeensyInterface(10)){
+    teensy->open();
 
-   add_actuator(PWMVelocityEffortChannelPtr(new PWMVelocityEffortChannel(robot_ns+"left_drive_actuator", as_interface, ae_interface)));
-   add_actuator(PWMVelocityEffortChannelPtr(new PWMVelocityEffortChannel(robot_ns+"right_drive_actuator", as_interface, ae_interface)));
+    add_actuator(PWMPositionEffortChannelPtr(new PWMPositionEffortChannel(robot_ns+"front_left_pod_joint_actuator", 0, 10, as_interface, ae_interface, teensy)));
+    add_actuator(PWMPositionEffortChannelPtr(new PWMPositionEffortChannel(robot_ns+"back_left_pod_joint_actuator", 1, 10, as_interface, ae_interface, teensy)));
+    add_actuator(PWMPositionEffortChannelPtr(new PWMPositionEffortChannel(robot_ns+"front_right_pod_joint_actuator", 2, 10, as_interface, ae_interface, teensy)));
+    add_actuator(PWMPositionEffortChannelPtr(new PWMPositionEffortChannel(robot_ns+"back_right_pod_joint_actuator", 3, 10, as_interface, ae_interface, teensy)));
 
-   std::vector<transmission_interface::TransmissionInfo> transmissions;
-   std::string urdf_string = river_ros_util::wait_for_param(n, "robot_description");
-   TransmissionParser::parse(urdf_string, transmissions);
-   build_transmissions(transmissions);
+    add_actuator(PWMVelocityEffortChannelPtr(new PWMVelocityEffortChannel(robot_ns+"left_drive_actuator", 4, 10, as_interface, ae_interface, teensy)));
+    add_actuator(PWMVelocityEffortChannelPtr(new PWMVelocityEffortChannel(robot_ns+"right_drive_actuator", 5, 10, as_interface, ae_interface, teensy)));
 
-   register_interfaces();
+    std::vector<transmission_interface::TransmissionInfo> transmissions;
+    std::string urdf_string = river_ros_util::wait_for_param(n, "robot_description");
+    TransmissionParser::parse(urdf_string, transmissions);
+    build_transmissions(transmissions);
+
+    register_interfaces();
   }
+
+ private:
+  WalrusTeensyInterfacePtr teensy;
   
 };
+
+#endif
