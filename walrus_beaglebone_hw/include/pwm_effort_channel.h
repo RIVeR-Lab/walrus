f281561d1a11afe7ceeadeacd642c9ae08b55c6e
Mitchell Wills <mwills@wpi.edu>
2014-04-11 21:27:26 -0400

    added specifying pot and encoder channels

--- walrus_beaglebone_hw/include/pwm_effort_channel.h
+++ walrus_beaglebone_hw/include/pwm_effort_channel.h
@@ -22,8 +22,6 @@ class PWMEffortChannelBase : public river_ros_util::RobotHWComponent{
 			   hardware_interface::ActuatorStateInterface& jsi,
 		       hardware_interface::EffortActuatorInterface& jei, WalrusTeensyInterfacePtr& teensy):
   cmd(0), pos(0), vel(0), eff(0), name(name), channel(channel), teensy(teensy), max_effort(max_effort){
-    ROS_INFO_STREAM("CREATED: "<<name);
-    printf("created\n");
 
     hardware_interface::ActuatorStateHandle state_handle(name, &pos, &vel, &eff);
     jsi.registerHandle(state_handle);
@@ -35,13 +33,11 @@ class PWMEffortChannelBase : public river_ros_util::RobotHWComponent{
   virtual void read() = 0;
 
   void write(){
-    ROS_INFO_THROTTLE(1, "HI");
     double actual_cmd = std::min(max_effort, std::max(cmd, -max_effort));
     uint16_t period_us = 1500 + us_per_torque*actual_cmd;
     teensy->set_pwm(channel, period_us);
     
     eff = actual_cmd;
-    ROS_INFO_THROTTLE(1, "BYE");
   }
 
  protected:
@@ -59,17 +55,19 @@ typedef boost::shared_ptr<PWMEffortChannelBase> PWMEffortChannelBasePtr;
 
 class PWMPositionEffortChannel : public PWMEffortChannelBase{
  public:
-  PWMPositionEffortChannel(std::string name, uint8_t channel, double max_effort,
+  PWMPositionEffortChannel(std::string name, uint8_t channel, uint8_t pot_channel, double max_effort,
 			   hardware_interface::ActuatorStateInterface& jsi,
 			   hardware_interface::EffortActuatorInterface& jei,
                            WalrusTeensyInterfacePtr& teensy):
-  PWMEffortChannelBase(name, channel, max_effort, jsi, jei, teensy)
+  PWMEffortChannelBase(name, channel, max_effort, jsi, jei, teensy), pot_channel(pot_channel)
 {}
 
-	virtual void read(){
-          uint16_t raw_position = teensy->get_analog(channel);
-          pos =  2*M_PI * raw_position / 1023;
-	}
+  virtual void read(){
+    uint16_t raw_position = teensy->get_analog(pot_channel);
+    pos =  2*M_PI * raw_position / 1023;
+  }
+ protected:
+  uint8_t pot_channel;
 };
 
 typedef boost::shared_ptr<PWMPositionEffortChannel> PWMPositionEffortChannelPtr;
@@ -77,19 +75,21 @@ typedef boost::shared_ptr<PWMPositionEffortChannel> PWMPositionEffortChannelPtr;
 
 class PWMVelocityEffortChannel : public PWMEffortChannelBase{
  public:
-  PWMVelocityEffortChannel(std::string name, uint8_t channel, double max_effort,
+  PWMVelocityEffortChannel(std::string name, uint8_t channel, uint8_t encoder_channel, double max_effort,
 			   hardware_interface::ActuatorStateInterface& jsi,
 			   hardware_interface::EffortActuatorInterface& jei,
                            WalrusTeensyInterfacePtr& teensy):
-  PWMEffortChannelBase(name, channel, max_effort, jsi, jei, teensy)
+  PWMEffortChannelBase(name, channel, max_effort, jsi, jei, teensy), encoder_channel(encoder_channel)
 {}
 
-	virtual void read(){
-          uint16_t raw_position = teensy->get_position(channel);
-          uint16_t raw_velocity = teensy->get_velocity(channel);
-          pos =  2*M_PI * raw_position / ticks_per_rotation;
-          vel =  2*M_PI * raw_velocity / ticks_per_rotation;
-	}
+  virtual void read(){
+    uint16_t raw_position = teensy->get_position(encoder_channel);
+    uint16_t raw_velocity = teensy->get_velocity(encoder_channel);
+    pos =  2*M_PI * raw_position / ticks_per_rotation;
+    vel =  2*M_PI * raw_velocity / ticks_per_rotation;
+  }
+ protected:
+  uint8_t encoder_channel;
 };
 
 
