ff9eb4d0a2c2494c517a569a8aa88bc3853b9831
Brian Eccles <brianjeccles@gmail.com>
2014-11-04 23:32:04 -0800

    Added new folder for arduino rosserial code.

---
+++ calf_arduino_rosserial/calf_arduino_rosserial.ino
@@ -0,0 +1,19 @@
+#include <ArduinoHardware.h>
+#include <ros.h>
+
+ros::NodeHandle  nh;
+
+void leftDriveCb(const std_msgs::Float64& drive_speed)
+{
+    s
+}
+ros::Subscriber<std_msgs::Float64> leftDrive("/calf/left_drive_controller/command", 
+
+setup ()
+{
+}
+
+loop ()
+{
+}
+
