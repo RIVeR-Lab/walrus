a00aa8a6f81c9e89b44576a43ff12d98b19e8c23
Mitchell Wills <mwills@wpi.edu>
2014-12-14 18:27:54 -0500

    Added support for loading from different machine (removed hardcoded localhost)

--- walrus_web/web/js/app.js
+++ walrus_web/web/js/app.js
@@ -2,10 +2,10 @@ var app = angular.module('app', ['ros', 'gamepad', 'ngMaterial']);
 
 app.config(["roslibProvider", "gamepadServiceProvider", "webrtcRosServiceProvider",
 	    function(roslibProvider, gamepadServiceProvider, webrtcRosServiceProvider){
-    roslibProvider.setRosbridgeWsUrl('ws://localhost:9003');
-    roslibProvider.setPackageUrl('http://localhost:9002/');
+    roslibProvider.setRosbridgeWsUrl('ws://'+location.hostname+':9003');
+    roslibProvider.setPackageUrl('http://'+location.hostname+':9002/');
 
-    webrtcRosServiceProvider.setSignalingUrl('ws://localhost:9001/webrtc');
+    webrtcRosServiceProvider.setSignalingUrl('ws://'+location.hostname+':9001/webrtc');
 
     gamepadServiceProvider.setPollRate(100);
 }]);
