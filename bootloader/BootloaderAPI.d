00d956fb924f7d607244ba667393a11a81126926
Brian Eccles <brianjeccles@gmail.com>
2015-01-04 00:01:23 -0500

    Added AT90USB1286 bootloader.

---
+++ bootloader/BootloaderAPI.d
@@ -0,0 +1,25 @@
+BootloaderAPI.o: BootloaderAPI.c BootloaderAPI.h LUFA/Common/Common.h \
+  LUFA/Common/Architectures.h LUFA/Common/BoardTypes.h \
+  LUFA/Common/ArchitectureSpecific.h LUFA/Common/CompilerSpecific.h \
+  LUFA/Common/Attributes.h Config/LUFAConfig.h LUFA/Common/Endianness.h \
+  Config/AppConfig.h
+
+BootloaderAPI.h:
+
+LUFA/Common/Common.h:
+
+LUFA/Common/Architectures.h:
+
+LUFA/Common/BoardTypes.h:
+
+LUFA/Common/ArchitectureSpecific.h:
+
+LUFA/Common/CompilerSpecific.h:
+
+LUFA/Common/Attributes.h:
+
+Config/LUFAConfig.h:
+
+LUFA/Common/Endianness.h:
+
+Config/AppConfig.h:
