eca0c24c7d10976be20e9f73c2d1a6831ac92d4a
Brian Eccles <brianjeccles@gmail.com>
2015-01-21 11:57:29 -0500

    Diagnostics board firmware.

---
+++ firmware/diagnostic_board_firmware/firmware/constants.h
@@ -0,0 +1,36 @@
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+//Status LED (pin sources LED current)
+#define P_LED_STATUS 0 //PE6
+
+//Button pins (normally open, grounded when closed)+
+#define P_BUTT_UP 0 	//PD6, Button 1
+#define P_BUTT_RIGHT 0	//PD5, Button 2
+#define P_BUTT_DOWN 0	//PE0, Button 3
+#define P_BUTT_LEFT 0 	//PD7, Button 4
+#define P_BUTT_CENT 0	//PE1, Button 5
+
+//Button LED pins (pins source LED current)
+#define P_LED_UP 0		//PB6, Button 1
+#define P_LED_RIGHT 0 	//PB7, Button 2
+#define P_LED_DOWN 0	//PC4, Button 3
+#define P_LED_LEFT 0	//PC5, Button 4
+#define P_LED_CENT 0	//PC6, Button 5
+
+//Display pins
+#define P_DISP_RS 0 //PF3
+#define P_DISP_RW 0 //PF4
+#define P_DISP_EN 0 //PF5
+#define P_DISP_D0 0 //PA0
+#define P_DISP_D1 0 //PA1
+#define P_DISP_D2 0 //PA2
+#define P_DISP_D3 0 //PA3
+#define P_DISP_D4 0 //PA4
+#define P_DISP_D5 0 //PA5
+#define P_DISP_D6 0 //PA6
+#define P_DISP_D7 0 //PA7
+
+
+#endif
---
+++ firmware/diagnostic_board_firmware/firmware/main.cpp
@@ -0,0 +1,143 @@
+
+#include <Arduino.h>
+#include <ros.h>
+#include <walrus_msgs/DiagnosticRXMsg.h>
+#include <walrus_msgs/DiagnosticTXMsg.h>
+#include "constants.h"
+#include "Bounce.h"
+#include "LiquidCrystal.h"
+ 
+//ROS node handle
+ros::NodeHandle nh;
+//ROS Message publisher
+walrus_msgs::DiagnositcRXMsg rx_msg;
+ros::Publisher rx("/walrus/diagnostic_board/rx", &rx_msg);
+//ROS Message subscriber
+ros::Subscriber<walrus_msgs::DiagnosticTXMsg> tx("/walrus/diagnostic_board/tx", &recv_msg);
+
+//LCD Object
+LiquidCrystal lcd(P_DISP_RS, P_DISP_RW, P_DISP_EN, P_DISP_D0, P_DISP_D1, P_DISP_D2, P_DISP_D3, P_DISP_D4, P_DISP_D5, P_DISP_D6, P_DISP_D7);
+//Bounce objects to debounce buttons
+Bounce up, right, down, left, cent;
+//Booleans to remember if a button was pressed
+bool last_up, last_right, last_down, last_cent, last_left;
+
+
+//Receive TX message from ROS master
+void recv_msg(const walrus_msgs::DiagnosticTXMsg& msg)
+{
+	//msg.display contains an array of 80 characters in row major order
+	//clear the screen and print them if they are not 0
+	lcd.clear();
+	for (int r = 0; r < 4; r++)
+	{
+		for (int c = 0; c < 20; c++)
+		{
+			if (msg.display[r*20+c] > 0)
+			{
+				lcd.setCursor(c, r);
+				lcd.write(msg.display[r*20+c];
+			}
+		}
+	}
+}
+
+void setup()
+{
+	//Initialize node, publishers and subscribers
+	nh.initNode();
+	nh.advertise(rx);	
+	nh.subscribe(tx);
+
+	//Setup LED pins as outputs and set them high
+	pinMode(P_LED_UP, OUTPUT);
+	pinMode(P_LED_RIGHT, OUTPUT);
+	pinMode(P_LED_DOWN, OUTPUT);
+	pinMode(P_LED_LEFT, OUTPUT);
+	pinMode(P_LED_CENT, OUTPUT);
+	digitalWrite(P_LED_UP, HIGH);
+	digitalWrite(P_LED_RIGHT, HIGH);
+	digitalWrite(P_LED_DOWN, HIGH);
+	digitalWrite(P_LED_LEFT, HIGH);
+	digitalWrite(P_LED_CENT, HIGH);
+	
+	//Setup LCD Object
+	lcd.noBlink();
+	lcd.noCursor();
+	
+	//Setup Buttons
+	up.interval(200);
+	down.interval(200);
+	right.interval(200);
+	left.interval(200);
+	cent.interval(200);
+	up.attach(P_BUTT_UP);
+	down.attach(P_BUTT_DOWN);
+	right.attach(P_BUTT_RIGHT);
+	left.attach(P_BUTT_LEFT);
+	cent.attach(P_BUTT_CENT);
+	last_up = false;
+	last_down = false;
+	last_right = false;
+	last_cent = false;
+	last_left = false;
+}
+
+
+
+void loop()
+{
+	//Update all buttons
+	up.update();
+	down.update();
+	right.update();
+	left.update();
+	cent.update();
+	
+	//If button is pressed, publish a message
+	if (up.read() && !last_up)
+	{
+		last_up = true;
+		rx_msg.button = 1;
+		rx.publish(&rx_msg);
+	}
+	if (right.read() && !last_right)
+	{
+		last_down = true;
+		rx_msg.button = 2;
+		rx.publish(&rx_msg);
+	}
+	if (down.read() && !last_down)
+	{
+		last_up = true;
+		rx_msg.button = 3;
+		rx.publish(&rx_msg);
+	}
+	if (left.read() && !last_left)
+	{
+		last_left = true;
+		rx_msg.button = 4;
+		rx.publish(&rx_msg);
+	}
+	if (cent.read() && !last_cent)
+	{
+		last_cent = true;
+		rx_msg.button = 5;
+		rx.publish(&rx_msg);
+	}
+	
+	//Clear button pressed bools when a button is released
+	if (!up.read() && last_up)
+		last_up = false;
+	if (!right.read() && last_right)
+		last_right = false;
+	if (!down.read() && last_down)
+		last_down = false;
+	if (!left.read() && last_left)
+		last_left = false;
+	if (!cent.read() && last_cent)
+		last_cent = false;
+	
+	//Check for ros updates
+	nh.spinOnce();
+}
