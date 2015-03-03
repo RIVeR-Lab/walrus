914a05c16ec67cdeee35ac07c6a7dd303f37a438
Mitchell Wills <mwills@wpi.edu>
2015-03-03 00:20:31 -0500

    Updated walrus_web_server to angular-material 0.8.2

--- walrus_web_server/src/app.js
+++ walrus_web_server/src/app.js
@@ -10,7 +10,31 @@ app.config(function(roslibProvider, gamepadServiceProvider, webrtcRosServiceProv
 
     gamepadServiceProvider.setPollRate(100);
 
-    $mdThemingProvider.theme("normal")
+    $mdThemingProvider.definePalette("white", {
+	"0": "#ffffff",
+	"50": "#ffffff",
+	"100": "#ffffff",
+	"200": "#ffffff",
+	"300": "#ffffff",
+	"400": "#ffffff",
+	"500": "#ffffff",
+	"600": "#ffffff",
+	"700": "#ffffff",
+	"800": "#ffffff",
+	"900": "#ffffff",
+	"1000": "#ffffff",
+	"A100": "#ffffff",
+	"A200": "#ffffff",
+	"A400": "#ffffff",
+	"A700": "#ffffff",
+	"contrastDefaultColor": "dark",
+	"contrastLightColors": "600 700 800 900"
+    });
+
+    $mdThemingProvider.theme("sidepanel-tabs")
+	.primaryPalette("white");
+    $mdThemingProvider.theme("default")
 	.primaryPalette("blue")
-	.accentPalette("deep-purple");
+	.accentPalette("deep-orange")
+	.backgroundPalette("blue-grey");
 });
