b157e1aba688eb873b86f99cb1d4f5dde257c66d
Mitchell Wills <mwills@wpi.edu>
2015-03-14 03:01:37 -0400

    Split epos into seperate node from other epos hardware Now has it's own controller manager

---
+++ walrus_base_hw/src/walrus_base_epos_hw_node.cpp
@@ -0,0 +1,49 @@
+#include <ros/ros.h>
+#include <ros/spinner.h>
+#include "epos_hardware/epos_hardware.h"
+#include <controller_manager/controller_manager.h>
+#include <vector>
+#include "walrus_base_hw/realtime_rate.h"
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "walrus_base_epos_hw_node");
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+
+  double controller_rate;
+  pnh.param<double>("controller_rate", controller_rate, 10);
+
+
+  std::vector<std::string> epos_names;
+  epos_names.push_back("left_drive_actuator");
+  epos_names.push_back("right_drive_actuator");
+  epos_hardware::EposHardware robot(nh, pnh, epos_names);
+
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  ROS_INFO("Initializing Motors");
+  if(!robot.init()) {
+    ROS_FATAL("Failed to initialize motors");
+    return 1;
+  }
+  ROS_INFO("Motors Initialized");
+
+  controller_manager::ControllerManager cm(&robot, pnh);
+
+  walrus_base_hw::RealtimeRate rate(controller_rate);
+  while (ros::ok()) {
+    ros::Duration dt;
+    ros::Time now;
+    rate.beginLoop(&now, &dt);
+
+    robot.read();
+    cm.update(now, dt);
+    robot.write();
+
+    robot.update_diagnostics();
+
+    rate.sleep();
+  }
+  return 0;
+}
