683d6eacd14aa105169b93eb7f54b7fa616dc19e
Brian Eccles <brianjeccles@gmail.com>
2015-04-27 01:08:26 -0400

    Modified main board firmware and messages to remove motor control and current feedback

--- walrus_firmware/main_board_firmware/firmware/constants.h
+++ walrus_firmware/main_board_firmware/firmware/constants.h
@@ -2,68 +2,18 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-//ROS Diagnostics states
-#define STATE_OK 0
-#define STATE_WARN 1
-#define STATE_ERROR 2
-#define STATE_STALE 3
-
-//Status structure
-typedef struct Status {
-    const char* str;
-    int16_t code;
-    uint16_t blink_code;
-} Status;
-
-//Defined statuses
-Status STATUS_ENABLED = {"Enabled", STATE_OK, /*10_1010_1010*/0x2AA}; 
-Status STATUS_DISABLED = {"Disabled", STATE_OK, /*11_1110_0000*/0x3E0};
-Status STATUS_NO_CONTROL_DATA = {"No data", STATE_WARN, /*10_1010_0000*/0x2A0};
-Status STATUS_NO_CONNECTION = {"No connection", STATE_ERROR, /*10_1000_0000*/0x280};
+//Status blink rates
+#define STATUS_OK 0x2AA
+#define STATUS_NO_CONTROL_DATA 0x2A0
+#define STATUS_NO_CONNECTION 0x280
 
 //Status LED (pin sources LED current)
 #define P_LED_STATUS 12
 
-//SERVO MODE
-//Motor control pins (for servo PWM)
-#define P_MOTOR_1 25 //MCU Pin: PB5; MCU Output Compare: OCR1A; Silkscreen: Pod Motor Control - 1 / J28; Schematic: POD_MOTOR2
-#define P_MOTOR_2 16 //MCU Pin: PC6; MCU Output Compare: OCR3A; Silkscreen: LED Board Control - 6 / J32; Schematic: LED3
-#define P_MOTOR_3 27 //MCU Pin: PB7; MCU Output Compare: OCR1C; Silkscreen: Pod Motor Control - 2 / J31; Schematic: POD_MOTOR4
-#define P_MOTOR_4 26 //MCU Pin: PB6; MCU Output Compare: OCR1B; Silkscreen: Pod Motor Control - 4 / J30; Schematic: POD_MOTOR3
 //External LED pins (for analogWrite)
 #define P_EXT_LED_1 14 //MCU Pin: PC4; MCU Output Compare: OCR3C; Silkscreen: LED Board Control - 4 / J26; Schematic: LED1
 #define P_EXT_LED_2 15 //MCU Pin: PC5; MCU Output Compare: OCR3B; Silkscreen: LED Board Control - 5 / J29; Schematic: LED2
-#define P_EXT_LED_3 24 //MCU Pin: PB4; MCU Output Compare: OCR2A; Silkscreen: Pod Motor Control - 3 / J27; Schematic: POD_MOTOR1
-
-//SERIAL MODE
-#define P_FRONT_TX 25
-#define P_FRONT_RX 16
-#define P_BACK_TX 27
-#define P_BACK_RX 26
-#define MAKE_MOTOR_1_BYTE(power) (((int16_t)power-1500)+192)
-#define MAKE_MOTOR_2_BYTE(power) (((int16_t)power-1500)+64)
-#define MAKE_MOTOR_3_BYTE(power) (((int16_t)power-1500)+64)
-#define MAKE_MOTOR_4_BYTE(power) (((int16_t)power-1500)+192)
-
-//TIMER MODE
-//16 bit timer top value
-#define TIMER_TOP 10000
-//Motor control output compare registers
-#define REG_MOTOR_1 OCR1A
-#define REG_MOTOR_2 OCR3A
-#define REG_MOTOR_3 OCR1C
-#define REG_MOTOR_4 OCR1B
-//LED output compare registers
-#define SET_LED1(value) OCR3C = value*10
-#define SET_LED2(value) OCR3B = value*10
-#define SET_LED3(value) OCR2A = value/4
-
-
-//Current sensor pins (analog)
-#define P_CURRENT_1 4
-#define P_CURRENT_2 5
-#define P_CURRENT_3 6
-#define P_CURRENT_4 7
+#define P_EXT_LED_3 16 //MCU Pin: PB4; MCU Output Compare: OCR2A; Silkscreen: Pod Motor Control - 3 / J27; Schematic: POD_MOTOR1
 
 //Encoder pins (analog)
 #define P_ENCODER_1 0
@@ -121,17 +71,11 @@ Status STATUS_NO_CONNECTION = {"No connection", STATE_ERROR, /*10_1000_0000*/0x2
 #define STATUS_BLINK_LENGTH 10
 #define LED_TRIGGER(count) (count % (STATUS_BLINK_PERIOD/ROS_MSG_RATE) == 0)
 #define LED_SEGMENT(count) ((count % (STATUS_BLINK_PERIOD/ROS_MSG_RATE*STATUS_BLINK_LENGTH))/(STATUS_BLINK_PERIOD/ROS_MSG_RATE))
-#define HS_DATA_TIMEOUT 250
 #define CONTROL_DATA_TIMEOUT 1500
 
 //OneWire commands
 #define T_CONVERT 0x44
 #define READ_SCRATCH 0xBE
-
-//Current Sensing
-#define CURRENT_AVERAGE_SAMPLES 10
-#define CURRENT_ADC_MAX 1023 
-#define ADC_TO_mA(adcVal) (int16_t)((73300*(long)adcVal/CURRENT_ADC_MAX) - 36700) //See Pololu part #2453
 
 //Temperature sensor addresses
 #define TEMP_1_ADDR {0x10, 0x9D, 0x92, 0xE2, 0x02, 0x08, 0x00, 0x43}
@@ -147,9 +91,7 @@ Status STATUS_NO_CONNECTION = {"No connection", STATE_ERROR, /*10_1000_0000*/0x2
 
 //Error strings
 #define LOOP_TOO_LONG_ERROR "Main loop took longer than 20 ms."
-#define SLOW_REQUEST_ERROR "Cannot complete request when enabled."
 #define INVALID_REQUEST_ERROR "Received invalid control message type."
-#define NO_MOTOR_DATA_ERROR "Received no motor data when enabled, disabling..."
 
 //Data report states for low speed data aquisition state machine
 enum LowSpeedOpState { READ_TEMPHUMID, 
